4517ed92ffbe19c5c65d4efc4a3de491b2dbc2c6
Leman_Russ <igor2002vlad@mail.ru>
2023-07-08 02:39:17 +0500

    3

--- WpfApp5/Bot_Logic.cs
+++ WpfApp5/Bot_Logic.cs
@@ -6,11 +6,39 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace WpfApp5
 {
     internal class Bot_Logic
     {
+    
+        enum CallBackQuearyRequist
+        {
+            add,
+            remove,
+            subscribes
+        }
+        string requist;
+        public static InlineKeyboardMarkup CreateTextButtonsList()
+        {
+            Context context = new Context();
+            var list = context.crypteItems.ToList();
+            InlineKeyboardButton[][] buttons = new InlineKeyboardButton[list.Count-1][];
+            int count = 0;
+            for (int i = 1; i < context.crypteItems.Count() ; i ++)
+            {
+                buttons[count] = new[]
+                {
+                            InlineKeyboardButton.WithCallbackData(list[i].name, list[i].name)
+                };
+                count++;
+            }
+
+            return buttons;
+        }
+
         Context context = new Context();
         public Bot_Logic() 
         {
@@ -24,24 +52,71 @@ namespace WpfApp5
 
         private Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
         {
-            var mes = update.Message.Text;
-            if (context.users.Where(x => x.chatId == update.Message.Chat.Id).Count() == 0)
+           
+            var mes = "s";
+            if (update.Type != UpdateType.CallbackQuery)
             {
-                context.users.Add(new TgUser()
-                {
-                    name = update.Message.Chat.FirstName,
-                    chatId = update.Message.Chat.Id
-                });
-                context.SaveChanges();
-            }
-            switch (mes)
-            {
-                case "/start":
-                    client.SendTextMessageAsync(update.Message.Chat.Id, text: $"hello world {update.Message.Chat.FirstName}");
-                    break;
+                mes = update.Message.Text;
 
+                if (context.users.Where(x => x.chatId == update.Message.Chat.Id).Count() == 0)
+                {
+                    context.users.Add(new TgUser()
+                    {
+                        name = update.Message.Chat.FirstName,
+                        chatId = update.Message.Chat.Id
+                    });
+                    context.SaveChanges();
+                }
+                switch (mes.ToLower())
+                {
+                    case "/start":
+
+                        client.SendTextMessageAsync(update.Message.Chat.Id, text: $"hello world {update.Message.Chat.FirstName}\n" +
+                            $"выберите интересующие вас крипто валюты, они будут добавлены в ваш список отслеживаемых.\n" +
+                            $"Что бы снова вызвать это меню, введите 'Крипта'\n" +
+                            $"что бы просмотреть подписки введите 'подписки'\n" +
+                            $"Что бы удалить определенную крипту из интересов введите 'удалить'", replyMarkup: CreateTextButtonsList());
+                        break;
+                    case "крипта":
+                        requist = CallBackQuearyRequist.add.ToString();
+                        break;
+                    case "подписки":
+                        requist = CallBackQuearyRequist.subscribes.ToString();
+                        break;
+                    case "удалить":
+                        requist = CallBackQuearyRequist.remove.ToString();
+                        break;
+
+                }
             }
+            else
+            {
+                switch (requist)
+                {
+                    case "add":
+                        requist = CallBackQuearyRequist.add.ToString();
+                        break;
+                    case "subscribes":
+                        requist = CallBackQuearyRequist.subscribes.ToString();
+                        break;
+                    case "remove":
+                        requist = CallBackQuearyRequist.remove.ToString();
+                        break;
+
+                }
+            }
+            
+           
+           
+
             return Task.CompletedTask;
+        }
+        public static InlineKeyboardButton[][] sendKeyboard()
+        {
+            InlineKeyboardButton[][] buttons = new InlineKeyboardButton[1][];
+
+
+            return new InlineKeyboardButton[1][];
         }
         private async Task PollingErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken token)
         {
